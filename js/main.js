c65edbaf7a33a47d98d722e5ef77e17410dcb34c
CeciliaCaroline <nalubegac58@gmail.com>
2018-07-28 20:22:15 +0300

    Feat: Input mapbox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2VjaWxpYWNhcm9saW5lIiwiYSI6ImNqazJrcGlsejA2c3YzcXFna3FrZXJnM3gifQ.iIf8fwNW1KzOM3mLGImrag',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
