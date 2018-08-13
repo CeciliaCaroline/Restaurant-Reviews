7446e3234d5b25a5580cf2a37383cef2076a78d4
Unknown <nalubegac58@gmail.com>
2018-08-13 15:36:47 +0300

    Feat: Cache website assets

---
+++ serviceworker.js
@@ -0,0 +1,32 @@
+const cacheFiles = [
+    "/",
+    "./js/main.js",
+    "./js/dbhelper.js",
+    "./index.html",
+    "./restaurant.html",
+    "./images/",
+    "./css/styles.css",
+    "./css/responsive.css",
+    "./img/"
+  ];
+  
+  self.addEventListener("install", e => {
+    e.waitUntil(
+      caches.open("cachev1").then(cache => {
+        return cache.addAll(cacheFiles);
+      }).catch(error => console.log(error))
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.open("cachev1").then((cache) => {
+        return cache.match(event.request).then((response) => {
+          return response || fetch(event.request).then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
