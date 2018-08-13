d46a6df728d7c9043870bba4044836171dafd945
CeciliaCaroline <nalubegac58@gmail.com>
2018-08-13 16:03:39 +0300

    Correct image url for restaurant

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./images/${restaurant.photograph}`);
+    return (`Restaurant-Reviews/images/${restaurant.photograph}`);
   }
 
   // Restaurant image alt
